556c34e0b9fbd24cde85806749d9cd2dcb1ee528
RedAlf <francismirezDPL@gmail.com>
2023-12-15 11:44:58 -0500

    cambios con el json

--- utils/config.dart
+++ utils/config.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 final _map = _createJsonMap();
 
 Map<String,String> _createJsonMap(){
-  final map = jsonDecode('lib/utils/db_data.json');  
+  final map = jsonDecode('assets/bd/db_data.json');  
   return map['myDB'];
 }
 
